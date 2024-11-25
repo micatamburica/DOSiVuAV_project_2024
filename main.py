6105a12853acedc5217943f064870f466d730883
micatamburica <milicav2001@gmail.com>
2024-11-25 22:52:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+import sys
+
+print("Start of the program\n---------------------")
+
+print("Python: ", sys.version)
+print("OpenCV: ", cv2.__version__)
+
+print("End of the program\n---------------------")
