01b967a96dd70319969d58d424274f1d13d68387
micatamburica <milicav2001@gmail.com>
2024-11-25 23:07:50 +0100

    testing

--- main.py
+++ main.py
@@ -3,7 +3,4 @@ import sys
 
 print("Start of the program\n---------------------")
 
-print("Python: ", sys.version)
-print("OpenCV: ", cv2.__version__)
-
 print("End of the program\n---------------------")
