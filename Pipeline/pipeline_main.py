031347dde700f5bbdfa6b551bffba3f12f8aca53
micatamburica <milicav2001@gmail.com>
2024-11-27 23:51:41 +0100

    trying to do canny

--- Pipeline/pipeline_main.py
+++ Pipeline/pipeline_main.py
@@ -1,7 +1,18 @@
 import Calibration.calibration_correction as calib
+import image_processing
+import cv2
+import glob
 
 if __name__=="__main__":
     
     srcImgPath = 'test_images/'
+    outputPath = 'output/'
+    srcImgName = 'test6.jpg'
     
-    calib.distortion_correction(srcImgPath, 'whiteCarLaneSwitch.jpg', True)
+    # 1. Distortion correction
+    calib.distortion_correction(srcImgPath, srcImgName)
+    
+    img = cv2.imread(outputPath + 'undistort_' + srcImgName)
+    
+    # 2. Thresholded binary image
+    image_processing.detection(img, srcImgName)
